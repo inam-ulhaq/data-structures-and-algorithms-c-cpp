baaafc501fb2f7221d2a465e5c1686cb0a689243
Inam Ul Haq <inam88643@gmail.com>
2025-11-20 01:21:55 +0500

    Explination of array ADT is added

---
+++ 04-array-ADT/00-array-ADT.cpp
@@ -0,0 +1,18 @@
+// Title: Array as an Abstract Data Type
+// Author: Inam Ul Haq
+// Description:
+//	ADT is the representation of data along with the set of operations 
+//	performed on that data.
+//
+//	An Array ADT represents data stored in an array and the set of 
+//	operations that can be performed on that array.
+//
+//	Array is a basic data structure provided by every programming language, 
+//	so the representation of data is handled by the compiler. However, the 
+//	set of operations is not provided by the compiler. We are responsible 
+//	for implementing those operations.
+//
+//	Therefore, the array data structure combined with the operations 
+//	defined on it is called the Array ADT.
+
+
