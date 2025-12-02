8fd40a97e990a658939cb9327c36426f3f14f5db
Inam Ul Haq <inam88643@gmail.com>
2025-12-03 02:55:29 +0500

    Charactor set ASCII codes and unicodes fundamental explination is uploded

---
+++ 05-string/01-charactor-set-ASCII-codes-and-uni-codes.c
@@ -0,0 +1,46 @@
+ Title: Charactor Set , ASCII codes and Uni Codes
+ Author: Inam Ul Haq
+ Descirption
+ 	Charactor Set:
+ 		It is the set of charactors that are supported by a programming language.
+ 		And the set of charactor a supported by a programming language is same as 
+ 		the set of charactor supported by a computer. We know very well a computer
+ 		system work on binary numbers, so everything in computer is numbers.Than 
+ 		how that can support charactors?
+ 			Well computer dosn't support any charactor, Than how we make them work 
+ 			on charactors? We will define some set of numbers as charactor, so it's 
+ 			mean for every charactor we define some unique numaric value, so we give 
+ 			some numaric codes.
+ 	
+	ASCII Codes:	
+		For english alphabites for every charactor there are some codes define. Thos 
+		codes are standerd codes and every electronic machine follow that same set of
+		codes called Amarican Standard Codes for Information Interchange (ASCII).
+		These codes are given by Amarican National Standard Institute that is ANSI.
+		And these codes are ISO standard. And that is the reason every electronic device
+		follow this set code. So ASCII codes are for english and for every latter there 
+		is a code availble. i.e.
+		
+		For capital latters i.e. A=65, B=66, C=67, D=68 .......X=88, Y=89, Z=90.
+		For small latters i.e. a=97, b=98, c=99 d=100 .........x=120, y=121, z=122.
+		For numarical value ASCII codes are also there i.e. for 0=48, 1=49,2=50.... 9=57.
+		Basically every charactor that are looking on keybord is a charactor set. So for 
+		other symboles on the key bord is also ASCII codes i.e. for Enter=10, Space bare=13
+		and for Esc= 27. For every special symbole there also an ASSCI code exist.
+		Total 128 ASCII codes are there so it start form 0 to 127. To represent these
+		7 binary bits are sufficent i.e. 2 power 7 is equal to 128 which is range form 
+		0 to 127. While allocating memory in ram 7 bits can't allocat minimume one byte is
+		allocated i.e. 8 bits so that's why the charactor take 1 Byte, otherwise it can be
+		store in 7 bits.
+		
+		
+	Unicodes:
+		For other natural languages like chines, handi and japnes the codes are define and 
+		those are also ISO standard code and those codes are called as Unicodes. Like unicodes
+		for all the languages and english is also one the language so ASCII codes is a subset 
+		of unicodes. Unicodes take 2 bytes i.e. 16 bits due to unicodes are represented in 
+		hexadecimal numbers, every hexadecimal digit take four bits and unicode can be store 
+		in foure hexadecimal digits so it 4 * 4 bits which is equal to 16 bits i.e. 2 bytes.
+		i.e C03A is a Unicode. Unicode.org is a web site that can help us to see the unicodes
+		chats for every language.
+	
