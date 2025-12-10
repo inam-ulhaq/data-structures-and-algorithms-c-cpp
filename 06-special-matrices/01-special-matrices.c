b8c6d962f71033f90b0595fc408e09b6d5666d7d
Inam Ul Haq <inam88643@gmail.com>
2025-12-10 22:06:35 +0500

    special matrices explination is added

---
+++ 06-special-matrices/01-special-matrices.c
@@ -0,0 +1,16 @@
+ Title: Special Matrices
+ Author: Inam Ul Haq
+ Description:
+ 	Special matracies are square matrices and they are having more numbers of 
+ 	zeros,We can store and process only the nonzero elements, avoiding the storage 
+	of zero elements, so we can save space and computation time.
+	
+ Example:
+ 	1- Diagnal matrix
+ 	2- Lower triangular matrix
+ 	3- Upper triangular matrix
+ 	4- Symmatric matrix
+ 	5- tirdiagonal matrix
+ 	6- Band matrix
+ 	7- Toplitz matrix
+ 	8- sphars matrix
