f2b0971e69b39f8b7dfb08030d1708f20fcee611
Inam Ul Haq <inam88643@gmail.com>
2025-12-23 15:35:23 +0500

    Why we need linked list

---
+++ 08-linked-list/01-why-we-need-linked-list.c
@@ -0,0 +1,33 @@
+/*
+ Title: Intro to Link List
+ Author: Inam Ul Haq
+ Description:
+
+ 1) Why We Need Linked Lists:
+
+    At the very beginning, we used simple variables to store data,
+    which was sufficient for small amounts of information.
+
+    However, as data size increased, using individual variables 
+	  became inefficient and hard to manage.
+
+    To solve this, we started using arrays — a collection of elements
+    stored in contiguous memory. Arrays are excellent when the amount
+    of data is known in advance and fixed, as they allow fast access
+    using indices.
+
+    But arrays come with a major limitation: their size is fixed at
+    compile time. When the exact size of the data is unknown beforehand,
+    we often allocate either too little memory (causing overflow) or
+    too much (wasting memory). This leads to inefficient memory use.
+
+    To overcome these limitations, we need a dynamic data structure —
+    one that can grow or shrink at runtime as elements are added or 
+ 	  removed.
+
+    This is where the linked list comes in.
+
+    Linked lists allow dynamic memory management by linking nodes 
+	  together, enabling flexible data storage without wasting or running out of memory.
+
+*/
