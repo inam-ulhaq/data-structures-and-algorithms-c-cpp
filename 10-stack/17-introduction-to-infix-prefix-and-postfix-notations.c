91eb565346c687f19101ce4627079a233c7ec9e9
Inam Ul Haq <inam88643@gmail.com>
2026-02-10 20:25:17 +0500

    Introduction to infic, prefix and postfix notations

---
+++ 10-stack/17-introduction-to-infix-prefix-and-postfix-notations.c
@@ -0,0 +1,39 @@
+/*
+ Title: Infix, Prefix, and Postfix Notations
+ Author: Inam Ul Haq
+ Description:
+	There are three notations for writing an equation.
+
+	1- Infix Notation
+		This is the most commonly used notation, especially in mathematics.
+		In this notation, the operator is placed between the operands. If it is a
+		binary operator, the expression is written as operand operator operand.
+
+		Example:
+		a + b
+		This means a is added with b.
+		Here, a and b are operands, and + is the operator
+		placed between them.
+
+	2- Prefix Notation
+		This notation was introduced in computer science.
+		In prefix notation, the operator appears before the operands on which it
+		performs the operation.
+
+		For a binary expression, the format is:
+		operator operand1 operand2
+
+		Example:
+		+ab
+		This means added a with b.
+		Here, + is the operator, and a and b are the operands.
+
+	3- Postfix Notation
+		This notation is also used in computer science.
+		In postfix notation, the operands appear first, followed by the operator.
+	
+		For a binary expression, a + b is written as:
+		ab+
+		This means a with b is added.
+		Here, a and b are the operands, and + is the operator.
+*/
