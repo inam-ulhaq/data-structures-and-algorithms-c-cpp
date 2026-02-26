8fcf425c74cfe391c5648cf0c8946aaefc75c9f8
Inam Ul Haq <inamulhaq.engineer@gmail.com>
2026-02-26 23:59:46 +0500

    Introduction-to-queue-data-structure

---
+++ 11-queues/01-introdution-to-queue-data-structure.c
@@ -0,0 +1,17 @@
+/*
+ Title: Queues Data Structure
+ Author: Inam Ul Haq
+
+ Description:
+	A queue is a logical data structure that works on the principle of 
+	First In, First Out (FIFO). This means that the element inserted 
+	first is removed first.
+
+	In daily life, we encounter queues in many situations. For example, 
+	people standing in a line to receive services, or cars waiting in a 
+	queue to cross a toll booth.
+
+	A queue has two ends: the front end and the rear end.
+	-1) Insertion (enqueue) is performed at the rear end.
+	-2) Deletion (dequeue) is performed from the front end.
+*/
