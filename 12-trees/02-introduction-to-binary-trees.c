b572101f4080c60add1027be94305f55bd09995d
Inam Ul Haq <inamulhaq.engineer@gmail.com>
2026-04-03 19:54:53 +0500

    Introduction-of-binary-tree-uploaded

---
+++ 12-trees/02-introduction-to-binary-trees.c
@@ -0,0 +1,10 @@
+/*
+ Title: Introduction to Binary Tree
+ Author: Inam Ul Haq
+ Description:
+	A tree with maximume degree two is called binary tree. It mean's
+	every node can have maximume two childerns and at least zero. As 
+	there are only two children we can also give names to them i.e. 
+	left child and right child. The number of possible childes in binary
+	tree for a node can be 0 ,1 or 2.
+*/
