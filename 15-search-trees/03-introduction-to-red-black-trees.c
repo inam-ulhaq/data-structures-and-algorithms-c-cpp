50fcd06eaaa9feb437fb1e785dd7706b2afd230f
Inam Ul Haq <inamulhaq.engineer@gmail.com>
2026-04-16 23:58:42 +0500

    Introduction-to-red-black-trees

---
+++ 15-search-trees/03-introduction-to-red-black-trees.c
@@ -0,0 +1,13 @@
+/*
+ Title: Reb Black trees
+ Author: Inam Ul Haq
+ Description:
+	Red black trees are similar to AVL Trees. But the idea of red black trees 
+	is extended form 2-3-4 trees which is a type of b-tree. In red  black tree 
+	every node will have color either red or black. Root of the tree will always black.
+	NULLs are also taken as black. Number of balcks (nods or null) on a path form 
+	root to leaf are alwasy same. No two consecuitive red will be there. i.e the 
+	parent and child or red must black. Newly inserted node will always red.
+	The height of red black tree is manimume log n and maximume is double i.e. 
+	2log n.
+*/	
