4563de1749aaaf54808253867d1a2d91bdee6c29
Inam Ul Haq <inamulhaq.engineer@gmail.com>
2026-04-18 23:58:58 +0500

    Introduction-to-heap

---
+++ 16-heap/01-introduction-heap.c
@@ -0,0 +1,19 @@
+/*
+ Title: Introduction To Heap
+ Author: Inam Ul Haq
+ Description:
+	Heap is a complete binary tree, where every element is greater or equal to 
+	all it's decendents(for max heap) or every element is smaller or equal to 
+	all it's decendents(for min heap).
+	-> Height will not increase unnecessarily and it will log(n) always because
+	it complete binary tree, And that's why we implement it mostly in using array.
+	-> The height of the as it log(n) it dosn't mean that it will be useful for 
+	searching no it's not useful for searching
+	
+	Complete Binary Tree: A binary tree in which all the levels will be fully filled
+	and last level is filled form left to right. Or when we reprsent this in array
+	then there will be no vecant place between the elements.
+	
+*/
+	
+	
