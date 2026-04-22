ef7aebc9877763eb44a2766abb908761b300e38f
Inam Ul Haq <inamulhaq.engineer@gmail.com>
2026-04-22 23:54:56 +0500

    Criteria-for-analysing-sorts

---
+++ 17-sorting-techniques/01-sorting-techniques.c
@@ -0,0 +1,27 @@
+ Title: Criteria for Analysing Sorts
+ Author: Inam Ul haq
+ Description:
+	There are so many sorting techniques, and each one have there own way of working,
+	So we can not say one of the sorting technique is a best sorting technique. There 
+	are some criteria base on which we can compare them and chose suitble for our 
+	requirment we can select a particular sorting technique.
+	
+	So what is the criteria based on which we will compare them? 
+	1. Sorting techniques are analyised based on the number of comparisions. i.e a sorting
+	technique may be comparing the elements and if it is comparing then how many total 
+	comparision it is performing, So actually total time taken by a sorting technique depends 
+	on the number of comparisions only, So this number of comparision only decaide the the 
+	time complexity of a sorting technique.
+	
+	2. Number of swaps, for arranging them in order the the element have change their places, 
+	So the elements have to swap. So swaping is common process done in almost the sorting 
+	techniques. So how many swaps it has to performing? we will analyise how many swaps a sorting
+	techniques is performing.
+	
+	3. Adoptive, when we are trying to keep the elements in sorted order, So what happen when they are 
+	already in the sorted order? So as they are already in the sorted order they must take manimume
+	time or less amount of time because it dosn't have to re-arrange the elmenets. So if any sorting 
+	algorithem taking less amount of time over already sorted elements then we say that the algorithem
+	is adoptive. 
+	
+	4. Extra memroy, out of all the sorting algorithams some needs extra memory
