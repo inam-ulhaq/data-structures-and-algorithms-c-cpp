99f61b771dcb1a0835a44ca4a257312aa217c19d
Inam Ul Haq <inamulhaq.engineer@gmail.com>
2026-05-07 21:37:12 +0500

    Why-we-need-hashing

---
+++ 18-hashing/01-why-we-need-hashing.c
@@ -0,0 +1,16 @@
+ Title: Why We Need Hashing
+ Author: Inam Ul Haq
+ Description:
+ 	Hashing is useful for searching, so if you have the list of keys and we 
+ 	you want to search some keys form them then you can use hashing. But for
+ 	searching we have already some known procedures? yes, and those are 
+ 	linear search and binary search, and we know their time complexities also 
+ 	i.e for linear search it's O(n) and for binary search it's O(log n). So we 
+ 	know binary search is faster then linear search which take time log n, but 
+ 	we want faster then this, That take less time then log(n). So for this hashing
+ 	is introduced. So hashing will try to search a key in less than log (n) 
+ 	time. Then what time is less than log(n)? constant i.e O(1), dos hashing
+ 	will search in constant time? yes it will take time constant or less than 
+ 	log(n).
+ 	So hashing will try to search the keys less than log(n) time that is almost 
+ 	constant time.
